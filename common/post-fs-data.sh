d3aeee1f5fcceea41de7f2bc14f5dfa8c6abb17c
Nomelas <HynesJeff57@gmail.com>
2016-11-17 22:02:25 -0400

    added -n for no event trigger

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,3 +6,7 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
+/data/magisk/resetprop -n --delete ro.qc.sdk.audio.fluencetype
+/data/magisk/resetprop -n persist.audio.fluence.voicerec true
+/data/magisk/resetprop -n persist.audio.fluence.speaker false
+/data/magisk/resetprop -n use.voice.path.for.pcm.voip true
